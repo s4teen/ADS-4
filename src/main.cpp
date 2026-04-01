47effd6cd43756159af9ecc740cb19668bc92daa
s4teen <112770218+s4teen@users.noreply.github.com>
2026-04-01 21:03:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <vector>
+
+int countPairs1(int* arr, int len, int value);
+int countPairs2(int* arr, int len, int value);
+int countPairs3(int* arr, int len, int value);
 
 int main() {
-    return 0;
+  int len = 0;
+  int value = 0;
+  std::cin >> len >> value;
+
+  std::vector<int> arr(len);
+  for (int i = 0; i < len; ++i) {
+    std::cin >> arr[i];
+  }
+
+  std::cout << countPairs1(arr.data(), len, value) << '\n';
+  std::cout << countPairs2(arr.data(), len, value) << '\n';
+  std::cout << countPairs3(arr.data(), len, value) << '\n';
+
+  return 0;
 }
